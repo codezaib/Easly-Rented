e2c69ef51e3311d78b53f0ef56f16ab5d7263ae9
Muhammad Shahzaib <shahzaibkhan4582@gmail.com>
2025-07-16 19:25:19 +0500

    last

--- db/connect.js
+++ db/connect.js
@@ -8,10 +8,10 @@ const mysql = require("mysql2/promise");
 // import { orderItem } from "../models/orderItem.js";
 // import { productsListed } from "../models/productsListed.js";
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "easly-rented",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
